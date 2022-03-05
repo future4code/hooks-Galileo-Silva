e6b6305604daa0a3e5861fe3f095cf7502cda9ea
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2022-03-05 13:11:13 -0300

    update

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,29 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+const boasVindas = prompt("Boas vindas ao jogo BlackJack!")
+
+if(confirm("pergunta de sim ou não")) {
+	// o que fazer se o usuário clicar "ok"
+} else {
+	// o que fazer se o usuário clicar "cancelar"
+}
+
+const carta = comprarCarta(); 
+// Sorteia uma carta. Por exemplo, o rei de ouros
+
+console.log(carta.texto) 
+// imprime o texto da carta. Nesse caso: "K♦️"
+
+console.log(carta.valor) 
+// imprime o valor da carta (um número). Nesse caso: 10)
