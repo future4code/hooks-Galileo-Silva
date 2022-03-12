b8895979014e096e9193f314da55dfba61702329
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2022-03-12 19:12:20 -0300

    Update nossoJogo.js

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -24,8 +24,8 @@ if (jogo) {
     let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
     let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
 
-    console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
-    console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
+    console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} = ${pontuacaoUsuario}`)
+    console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} = ${pontuacaoPc}`)
     if (pontuacaoUsuario > pontuacaoPc) {
         console.log("O usuário ganhou!")
     } else if (pontuacaoPc > pontuacaoUsuario) {
@@ -40,8 +40,8 @@ if (jogo) {
 const carta = comprarCarta();
 // Sorteia uma carta. Por exemplo, o rei de ouros
 
-console.log(carta.texto)
-    // imprime o texto da carta. Nesse caso: "K♦️"
+//console.log(carta.texto)
+// imprime o texto da carta. Nesse caso: "K♦️"
 
-console.log(carta.valor)
-    // imprime o valor da carta (um número). Nesse caso: 10)
+//console.log(carta.valor)
+// imprime o valor da carta (um número). Nesse caso: 10)
