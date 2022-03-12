0b9274474630bb3ea2a97a5d76677f2daa77be23
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2022-03-12 19:08:26 -0300

    Update nossoJogo.js

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -11,19 +11,37 @@
  * 
  */
 
-const boasVindas = prompt("Boas vindas ao jogo BlackJack!")
+console.log("Boas vindas ao jogo BlackJack!")
 
-if(confirm("pergunta de sim ou não")) {
-	// o que fazer se o usuário clicar "ok"
+let jogo = confirm("Quer jogar uma nova rodada?")
+
+if (jogo) {
+    let carta1Usuario = comprarCarta()
+    let carta2Usuario = comprarCarta()
+    let carta1Pc = comprarCarta()
+    let carta2Pc = comprarCarta()
+
+    let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+    let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
+
+    console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
+    console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
+    if (pontuacaoUsuario > pontuacaoPc) {
+        console.log("O usuário ganhou!")
+    } else if (pontuacaoPc > pontuacaoUsuario) {
+        console.log("O computador ganhou!")
+    } else if (pontuacaoUsuario === pontuacaoPc) {
+        console.log("Empate")
+    }
 } else {
-	// o que fazer se o usuário clicar "cancelar"
+    console.log("O jogo acabou")
 }
 
-const carta = comprarCarta(); 
+const carta = comprarCarta();
 // Sorteia uma carta. Por exemplo, o rei de ouros
 
-console.log(carta.texto) 
-// imprime o texto da carta. Nesse caso: "K♦️"
+console.log(carta.texto)
+    // imprime o texto da carta. Nesse caso: "K♦️"
 
-console.log(carta.valor) 
-// imprime o valor da carta (um número). Nesse caso: 10)
+console.log(carta.valor)
+    // imprime o valor da carta (um número). Nesse caso: 10)
