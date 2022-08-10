3b3b947ff27eb0ed6afde51f4ff1c0b90a1c223d
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2022-08-10 11:56:26 -0300

    corrigindo o exercicio

--- bloco14/intro-typescript/exercicio2.ts
+++ bloco14/intro-typescript/exercicio2.ts
@@ -1,13 +1,6 @@
-<<<<<<< Updated upstream
-function imprimeTresCoresFavoritas(): void {
-    const cor1: string = prompt("Insira sua primeira cor favorita")
-    const cor2: string = prompt("Insira sua segunda cor favorita")
-    const cor3: string = prompt("Insira sua terceira cor favorita")
-=======
 function imprimeTresCoresFavoritas() {
     const cor1: any = prompt("Insira sua primeira cor favorita")
     const cor2: any = prompt("Insira sua segunda cor favorita")
     const cor3: any = prompt("Insira sua terceira cor favorita")
->>>>>>> Stashed changes
     console.log([cor1, cor2, cor3])
  }
