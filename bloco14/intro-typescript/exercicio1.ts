7ae211b05193edd93609e9dbc2526ab84c37de16
GalileoGuilherme <76979588+GalileoGuilherme@users.noreply.github.com>
2022-08-10 11:50:46 -0300

    completando o exercicio

--- bloco14/intro-typescript/exercicio1.ts
+++ bloco14/intro-typescript/exercicio1.ts
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 function checaTriangulo(a: string, b: string, c: string): string {
     if (a !== b && b !== c)  {
+=======
+function checaTriangulo(a: string, b: string, c: string): string{
+    if (a !== b && b !== c) {
+>>>>>>> Stashed changes
       return "Escaleno";
     } else if (a === b && b === c) {
       return "Equilátero";
